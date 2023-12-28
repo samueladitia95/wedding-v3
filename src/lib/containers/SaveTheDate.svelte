1865a416e19a4cdd9b516df21cbd2b826f56c36e
Samuel Aditia <samueladitia95@gmail.com>
2023-12-28 20:10:32 +0700

    update

--- src/lib/containers/SaveTheDate.svelte
+++ src/lib/containers/SaveTheDate.svelte
@@ -40,7 +40,7 @@
 </script>
 
 <div
-	class="bg-ca-red h-screen overflow-hidden"
+	class="bg-ca-red min-h-screen w-screen overflow-hidden relative xl:min-h-[120vh]"
 	use:inview={{
 		rootMargin: "0px",
 		unobserveOnEnter: true,
@@ -49,37 +49,35 @@
 	id="save-the-date"
 >
 	{#if isShow}
-		<div class="h-full w-full relative z-10">
+		<div
+			class="container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-6 w-full md:mt-10 xl:max-w-xl"
+			in:fade={{ duration: 1000, delay: 500 }}
+		>
 			<div
-				class="container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center gap-6 w-full md:mt-10 xl:max-w-xl"
-				in:fade={{ duration: 1000, delay: 500 }}
+				class="text-ca-creme font-homemade-apple text-3xl/loose md:text-5xl/loose -rotate-12 absolute left-8 -top-28 md:-top-32 xl:-left-20 xl:-top-20"
 			>
-				<div
-					class="text-ca-creme font-homemade-apple text-3xl/loose md:text-5xl/loose -rotate-12 absolute left-8 -top-28 md:-top-32 xl:-left-20 xl:-top-20"
-				>
-					<div>Save the date!</div>
-					<div>{weddingDay.format("DD MMM YYYY")}</div>
-				</div>
-				<img
-					src={saveTheDateImage}
-					alt="Save Them Date"
-					class="h-64 md:h-[586px] lg:h-[502px] w-full object-cover rounded"
-				/>
-				<div
-					class="text-ca-creme font-editor-hand text-2xl/loose md:text-4xl/loose absolute bottom-28 -right-2 md:right-4 -rotate-12"
-				>
-					{groomAndBrideName}
-				</div>
-				<div
-					class="text-ca-creme font-editor-hand flex justify-between gap-5 pt-10 md:pt-12 md:w-full md:px-10 lg:px-0"
-				>
-					{#each countdowns as countdown}
-						<div class="flex flex-col justify-center items-center">
-							<div class=" text-3xl md:text-5xl">{countdown.value}</div>
-							<div class="text-sm md:text-2xl">{countdown.type}(S)</div>
-						</div>
-					{/each}
-				</div>
+				<div>Save the date!</div>
+				<div>{weddingDay.format("DD MMM YYYY")}</div>
+			</div>
+			<img
+				src={saveTheDateImage}
+				alt="Save Them Date"
+				class="h-64 md:h-[586px] lg:h-[502px] w-full object-cover rounded"
+			/>
+			<div
+				class="text-ca-creme font-editor-hand text-2xl/loose md:text-4xl/loose absolute bottom-28 -right-2 md:right-4 -rotate-12"
+			>
+				{groomAndBrideName}
+			</div>
+			<div
+				class="text-ca-creme font-editor-hand flex justify-between gap-5 pt-10 md:pt-12 md:w-full md:px-10 lg:px-0"
+			>
+				{#each countdowns as countdown}
+					<div class="flex flex-col justify-center items-center">
+						<div class=" text-3xl md:text-5xl">{countdown.value}</div>
+						<div class="text-sm md:text-2xl">{countdown.type}(S)</div>
+					</div>
+				{/each}
 			</div>
 		</div>
 	{/if}
